cb0f03a0b0cdff9ed76d234161071779033803b5
mobak88 <mostafa0401@ymail.com>
2022-02-09 18:03:21 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,63 @@
+const key = 'key';
+const geocodeKey = 'key';
+const lang = 'en';
+const units = 'metric';
+let userInput = document.getElementById('search');
+
+const displayCity = document.querySelector('.city-name');
+let displayTemp = document.querySelector('.city-temp');
+
+let API = `https://api.opencagedata.com/geocode/v1/json?key=${geocodeKey}&q=oslo}`;
+
+async function getCityCoords() {
+  try {
+    const response = await fetch(API);
+    const data = await response.json();
+    createWeatherData(
+      data.results[0].geometry.lat,
+      data.results[0].geometry.lng
+    );
+    console.log(data);
+    console.log(data.results[0].geometry.lat);
+    console.log(data.results[0].geometry.lng);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createWeatherData(lat, lon) {
+  let weatherData = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${key}&units=${units}&lang=${lang}`;
+
+  async function getWeatherData() {
+    try {
+      const response = await fetch(weatherData);
+      const data = await response.json();
+      getCity(data);
+      displayCityTemp(data);
+    } catch (err) {
+      console.log(err);
+    }
+  }
+  getWeatherData();
+}
+
+function getCity(data) {
+  const city = data.timezone.replace('Europe/', '');
+  displayCityName(city);
+}
+
+function displayCityName(cityName) {
+  displayCity.innerHTML = cityName;
+}
+
+function displayCityTemp(data) {
+  displayTemp.innerHTML = `${Math.round(data.daily[0].temp.day)}°`;
+  console.log(data.daily[0].temp.day);
+}
+
+function pushDataToArr(data, city, temp) {
+  data.push({
+    city: city,
+    temp: temp,
+  });
+}
